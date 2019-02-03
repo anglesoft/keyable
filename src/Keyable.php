74715a4f0438b4eb9b3c0cf574aae24d3f86ff1d
gotfunc <benoit.villiere@gmail.com>
2019-02-03 17:04:32 +0100

    Added comments

--- src/Keyable.php
+++ src/Keyable.php
@@ -8,13 +8,19 @@ use Illuminate\Support\Str;
 
 trait Keyable
 {
-    public static function bootKeyable()
+    /**
+     * Boots the Keyable trait.
+     */
+    public static function bootKeyable() : void
     {
         static::creating(function ($model) {
             $model->assignUniqueKeys();
         });
     }
 
+    /**
+     * Assign keys to the current model instance.
+     */
     public function assignUniqueKeys() : void
     {
         if ( ! property_exists($this, 'keys')) {
@@ -30,30 +36,60 @@ trait Keyable
         }
     }
 
+    /**
+     * Generates a random string.
+     *
+     * @param  string   $attribute
+     * @param  integer  $length
+     * @return string
+     */
     public function keyableStrategy(string $attribute, int $length = 32) : string
     {
         return Str::random($length);
     }
 
-    public function generateUniqueKey(string $attribute, int $length)
+    /**
+     * Generates a unique key against the current table.
+     *
+     * @param  string   $attribute
+     * @param  int      $length
+     * @return string
+     */
+    public function generateUniqueKey(string $attribute, int $length) : string
     {
         $table = $this->getTable();
 
         do {
             $key = $this->keyableStrategy($attribute, $length);
-        } while (DB::table($table)->where($attribute, $key)->first());
+        } while (DB::table($table)->where($attribute, $key)->exists());
 
         return $key;
     }
 
-    static function findByKey(string $key) : ?Model
+    /**
+     * Find a model by its key.
+     *
+     * @param  string $attribute
+     * @param  string $value
+     * @return Model|null
+     */
+    static function findByKey(string $attribute, string $value) : ?Model
     {
-        return static::where('key', $key)->first();
+        return static::where($attribute, $value)->first();
     }
 
-    static function findByKeyOrFail(string $key, $code = 404) : ?Model
+    /**
+     * Find a model by its key or throw an exception.
+     *
+     * @param  string   $key
+     * @param  integer  $code
+     * @return Model|void
+     *
+     * @throws \Symfony\Component\HttpKernel\Exception\HttpException
+     */
+    static function findByKeyOrFail(string $attribute, string $value, int $code = 404)
     {
-        $model = static::findByKey($key);
+        $model = static::findByKey($attribute, $value);
 
         if ( ! $model) {
             return abort($code);
